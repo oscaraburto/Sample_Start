478a5d4a7a232dad1db2960756253df7e35b7c01
oscaraburto <31790090+oscaraburto@users.noreply.github.com>
2017-11-24 02:07:11 -0800

    Add files via upload

--- website/script-00.js
+++ website/script-00.js
@@ -1,41 +1,42 @@
-
+// Global Variables
 const candidates = [
     { fname: 'Peter Parker', nlikes: 700 },
     { fname: 'Tony Stark', nlikes: 135 },
     { fname: 'dash', nlikes: 0 },
 ]
 
-
+// Functions
 function funConst() {
+    document.write("<table class='table'><thead><tr><th>Full Name | </th><th># of Likes | </th><th> Like |</th><th> Delete |</th></tr></thead><tbody>");
     for (var i = 0; i < candidates.length; i++) {
-       //document.getElementById("box-1-p").innerHTML = 
-       document.write("<tr id='perRow" + i +"'><td>" + candidates[i].fname + "</td><td id='c"+i+"'>" 
-                + candidates[i].nlikes + "</td>"
-                + '<td><button type="button" onclick="addLike('+ i +')"><i class="glyphicon glyphicon-thumbs-up" style="font-size: 22px; color: rgb(30, 143, 1)"></i></button></td>'
-                + '<td><button type="button" onclick="delRow(' + i +')"><i class="glyphicon glyphicon-remove" style="font-size: 22px; color: red;"></i></button></td></tr>');
+        document.write("<tr id='perRow" + i + "'><td>" + candidates[i].fname + "</td><td id='c" + i + "'>"
+            + candidates[i].nlikes + "</td>"
+            + '<td><button type="button" onclick="addLike(' + i + ')"><i class="glyphicon glyphicon-thumbs-up" style="font-size: 22px; color: rgb(30, 143, 1)"></i></button></td>'
+            + '<td><button type="button" onclick="delRow(' + i + ')"><i class="glyphicon glyphicon-remove" style="font-size: 22px; color: red;"></i></button></td></tr>');
     }
-}                            
+    document.write("</tbody></table>");
+}
 
 function addLike(arg1) {
     candidates[arg1].nlikes += 1;
-    document.getElementById("c"+[arg1]).innerHTML = candidates[arg1].nlikes;
+    document.getElementById("c" + [arg1]).innerHTML = candidates[arg1].nlikes;
 }
 
 function delRow(arg1) {
     candidates[arg1].fname = null;
-    candidates[arg1].nlikes= null;
-    document.getElementById("perRow"+[arg1]).innerHTML = null;
+    candidates[arg1].nlikes = null;
+    document.getElementById("perRow" + [arg1]).innerHTML = null;
+}
+
+function addPer() {
+    var userName = document.getElementById("userName").value;
+    candidates.push({ fname: userName, nlikes: 0 });
+    document.getElementById("box-1");
 }
 
 // Start
-document.write("<table class='table'><thead><tr><th>Full Name | </th><th># of Likes | </th><th> Like |</th><th> Delete |</th></tr></thead><tbody>");
 funConst();
-document.write("</tbody></table>");
 
 
 
 
-/*    for (var i = 0; i < candidates.length; i++) {
-    return candidates[i].fname + ' ' + candidates[i].nlikes;
-    
-        */
